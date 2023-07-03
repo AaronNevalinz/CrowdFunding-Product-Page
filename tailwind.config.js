a37583b5083cae99a87ec5b0e1253ad138144260
Aaron Nevalinz <aaronug421@gmail.com>
2023-07-03 16:41:13 +0300

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./public/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:{
+          100:'hsl(176, 50%, 47%)',
+          200:'hsl(176, 72%, 28%)'
+        },
+        neutral:{
+          100:'hsl(0, 0%, 48%)'
+        }
+      }
+    },
   },
   plugins: [],
 }
