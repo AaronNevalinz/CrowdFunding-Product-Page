ff3599dd395e4cc061a382e455f76dca66c49ba3
AaronNevalinz <aaronug421@gmail.com>
2023-07-08 12:20:51 +0300

    popup modified

--- public/popup.js
+++ public/popup.js
@@ -2,12 +2,15 @@
     let backupBtn = document.querySelector('#backup-btn');
     let backupPopup = document.querySelector('#backup-popup');
     let popupHidden = document.querySelector('#popup-hide');
+    let content = document.querySelector('#all-content');
 
     backupBtn.addEventListener('click', ()=>{
         backupPopup.classList.add('visible');
+        content.classList.toggle('blur-bg')
         backupPopup.classList.remove('hidden')
     });
 
     popupHidden.addEventListener('click', ()=>{
         backupPopup.classList.add('hidden');
+        content.classList.remove('blur-bg')
     });
