9d32458c318ba42bc0b2d2e6fbf8c6e067a6f1dc
AaronNevalinz <aaronug421@gmail.com>
2023-07-11 18:28:03 +0300

    modified

--- public/popup.js
+++ public/popup.js
@@ -22,26 +22,84 @@
         content.classList.remove('h-[100vh]');
     });
 
-    let pledgeCard = document.querySelector('#pledge-card');
-    let pledgeAmount = document.querySelector('#enter-pledge-amount');
-    let continueButton = document.querySelector('#continue-btn');
+    // let pledgeCard = document.querySelector('#pledge-card');
+    // let pledgeAmount = document.querySelector('#enter-pledge-amount');
+    // let continueButton = document.querySelector('#continue-btn');
 
-    pledgeCard.addEventListener('click', ()=>{
-        pledgeCard.classList.add('border-primary-100')
-        pledgeAmount.classList.remove('hidden');
+    // pledgeCard.addEventListener('click', ()=>{
+    //     pledgeCard.classList.add('border-primary-100')
+    //     pledgeAmount.classList.remove('hidden');
+    // });
+
+    // continueButton.addEventListener('click', ()=>{
+    //     backupPopup.classList.add('visible');
+    //     overlayDiv.classList.remove('hidden');
+    //     backupPopup.classList.remove('hidden');
+    //     content.classList.add('overflow-hidden');
+    //     content.classList.add('h-[100vh]');
+    //     backProjectPopUp.classList.add('hidden')
+    // });
+
+    // popupHidden.addEventListener('click', ()=>{
+    //     backupPopup.classList.add('hidden');
+    //     overlayDiv.classList.add('hidden');
+    //     content.classList.remove('h-[100vh]');
+    // });
+
+
+    const cards = document.querySelectorAll('.pledge-card');
+    // cards.forEach(card=>{
+        
+    //     const pledgeAmount = document.querySelector('#enter-pledge-amount');
+    //     const continueButton = document.querySelector('#continue-btn');
+    //     const backProjectPopUp = document.querySelector('#back-this-project-popup');
+    //     const backupPopup = document.querySelector('#thank-you-backup-popup');
+    //     const popupHidden = document.querySelector('#popup-hide');
+    //     const overlayDiv = document.querySelector('#overlay-div');
+
+    //     card.addEventListener('click', ()=>{
+    //         card.classList.add('border-primary-100');
+    //         pledgeAmount.classList.remove('hidden');
+    //     });
+
+    //     continueButton.addEventListener('click', ()=>{
+    //         backProjectPopUp.classList.add('hidden');
+    //         backupPopup.classList.remove('hidden');
+    //     });
+
+    //     popupHidden.addEventListener('click', ()=>{
+    //         backupPopup.classList.add('hidden');
+    //         overlayDiv.classList.add('hidden');
+    //     });
+    // });
+
+    $('.pledge-card').click(()=>{
+        $(this).addClass('border-primary-100').siblings().removeClass('border-primary-100');
     });
 
-    continueButton.addEventListener('click', ()=>{
-        backupPopup.classList.add('visible');
-        overlayDiv.classList.remove('hidden');
-        backupPopup.classList.remove('hidden');
-        content.classList.add('overflow-hidden');
-        content.classList.add('h-[100vh]');
-        backProjectPopUp.classList.add('hidden')
-    });
 
-    popupHidden.addEventListener('click', ()=>{
-        backupPopup.classList.add('hidden');
-        overlayDiv.classList.add('hidden');
-        content.classList.remove('h-[100vh]');
-    });
+
+
+
+
+
+
+
+    // const backProjectContainer = document.getElementById('back-this-project-popup');
+    // const cards = document.querySelectorAll('.pledge-card')
+
+    // let selectedElement = null;
+
+    // backProjectContainer.addEventListener('click', function(event){
+    //     if(event.target.classList.contains('pledge-card')){
+    //         const clickedElement = event.target;
+
+    //         if(selectedElement !== null){
+    //             selectedElement.classList.remove('border-primary-100');
+    //         }
+
+    //         clickedElement.classList.add('border-primary-200'); 
+
+    //         selectedElement = clickedElement;
+    //     }
+    // });
